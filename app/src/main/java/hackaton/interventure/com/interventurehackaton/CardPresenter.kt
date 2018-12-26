93f638bf4926abf666619baf9a7d8099cb4484c5
Nikola Petrovic <nikola.petrovic@redbox.com>
2018-12-26 12:00:54 +0100

    add interventure color

--- app/src/main/java/hackaton/interventure/com/interventurehackaton/CardPresenter.kt
+++ app/src/main/java/hackaton/interventure/com/interventurehackaton/CardPresenter.kt
@@ -38,7 +38,7 @@ class CardPresenter : Presenter() {
         Log.d(TAG, "onCreateViewHolder")
 
         sDefaultBackgroundColor = ContextCompat.getColor(parent.context, R.color.default_background)
-        sSelectedBackgroundColor = ContextCompat.getColor(parent.context, R.color.selected_background)
+        sSelectedBackgroundColor = ContextCompat.getColor(parent.context, R.color.fastlane_background)
         mDefaultCardImage = ContextCompat.getDrawable(parent.context, R.drawable.movie)
 
         val cardView = object : ImageCardView(parent.context) {
