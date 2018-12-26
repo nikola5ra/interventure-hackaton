1ded05643353516dda291254327f29a30eb5afc0
Nikola Petrovic <nikola.petrovic@redbox.com>
2018-12-26 15:40:59 +0100

    Change interventure background image

--- app/src/main/java/hackaton/interventure/com/interventurehackaton/main/PageRowFragmentFactory.kt
+++ app/src/main/java/hackaton/interventure/com/interventurehackaton/main/PageRowFragmentFactory.kt
@@ -5,6 +5,8 @@ import android.support.v17.leanback.app.BackgroundManager
 import android.support.v17.leanback.app.BrowseSupportFragment
 import android.support.v17.leanback.widget.Row
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
+import hackaton.interventure.com.interventurehackaton.R
 import hackaton.interventure.com.interventurehackaton.main.events.EventsFragment
 import hackaton.interventure.com.interventurehackaton.main.interventure.InterventureFragment
 import hackaton.interventure.com.interventurehackaton.main.people.PeopleFragment
@@ -17,7 +19,11 @@ internal class PageRowFragmentFactory internal constructor(
 
     override fun createFragment(rowObj: Any?): Fragment {
         val row = rowObj as Row
-        mBackgroundManager.drawable = null
+        if (row.headerItem.id == MainFragment.HEADER_INTERVENTURE_ID) {
+            mBackgroundManager.drawable = ContextCompat.getDrawable(context, R.drawable.interventure_background)
+        } else {
+            mBackgroundManager.drawable = null
+        }
         return when {
             row.headerItem.id == MainFragment.HEADER_INTERVENTURE_ID -> InterventureFragment()
             row.headerItem.id == MainFragment.HEADER_TEAMS_ID -> TeamFragment()
